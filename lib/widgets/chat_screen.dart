a4b0d8863ca655f6740922ed85766fb7354a201b
Zhiyi Jin <victoryjzy@gmail.com>
2023-06-10 15:21:53 -0700

    Add Markdown, LaTeX syntax support.

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -1,18 +1,19 @@
 import 'package:chatgpt/injection.dart';
+import 'package:chatgpt/markdown/latex.dart';
 import 'package:chatgpt/models/message.dart';
 import 'package:chatgpt/states/chat_ui_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:markdown_widget/markdown_widget.dart';
 
+import '../markdown/code_wrapper.dart';
 import '../states/messge_state.dart';
 
 class ChatScreen extends HookConsumerWidget {
-  final _textController = TextEditingController();
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final chatUIState = ref.watch(chatUiProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
@@ -20,69 +21,17 @@ class ChatScreen extends HookConsumerWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          children: [
-            const Expanded(
+          children: const [
+            Expanded(
               // 聊天消息列表
               child: ChatMessageList(),
             ),
             // 输入框
-            TextField(
-              enabled: !chatUIState.requestLoading,
-              controller: _textController,
-              decoration: InputDecoration(
-                  hintText: 'Type a message', // 显示在输入框内的提示文字
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      // 这里处理发送事件
-                      if (_textController.text.isNotEmpty) {
-                        _sendMessage(ref, _textController.text);
-                      }
-                    },
-                    icon: const Icon(
-                      Icons.send,
-                    ),
-                  )),
-            ),
+            UserInputWidget(),
           ],
         ),
       ),
     );
-  }
-
-  // 增加WidgetRef
-  _sendMessage(WidgetRef ref, String content) {
-    final message = Message(
-      id: uuid.v4(),
-      content: content,
-      isUser: true,
-      timestamp: DateTime.now(),
-    );
-    ref.read(messageProvider.notifier).upsertMessage(message); // 添加消息
-    _textController.clear();
-    _requestChatGPT(ref, content);
-  }
-
-  _requestChatGPT(WidgetRef ref, String content) async {
-    ref.read(chatUiProvider.notifier).setRequestLoading(true);
-    try {
-      final id = uuid.v4();
-      await chatgpt.streamChat(
-        content,
-        onSuccess: (text) {
-          final message = Message(
-            id: id,
-            content: text,
-            isUser: false,
-            timestamp: DateTime.now(),
-          );
-          ref.read(messageProvider.notifier).upsertMessage(message);
-        },
-      );
-    } catch (err) {
-      logger.e("requestChatGPT error: $err", err);
-    } finally {
-      ref.read(chatUiProvider.notifier).setRequestLoading(false);
-    }
   }
 }
 
@@ -116,6 +65,70 @@ class ChatMessageList extends HookConsumerWidget {
   }
 }
 
+class UserInputWidget extends HookConsumerWidget {
+  const UserInputWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final chatUIState = ref.watch(chatUiProvider);
+    final controller = useTextEditingController();
+    return TextField(
+      enabled: !chatUIState.requestLoading,
+      controller: controller,
+      decoration: InputDecoration(
+          hintText: 'Type a message', // 显示在输入框内的提示文字
+          suffixIcon: IconButton(
+            onPressed: () {
+              // 这里处理发送事件
+              if (controller.text.isNotEmpty) {
+                _sendMessage(ref, controller);
+              }
+            },
+            icon: const Icon(
+              Icons.send,
+            ),
+          )),
+    );
+  }
+
+  // 增加WidgetRef
+  _sendMessage(WidgetRef ref, TextEditingController controller) async {
+    final content = controller.text;
+    final message = Message(
+      id: uuid.v4(),
+      content: content,
+      isUser: true,
+      timestamp: DateTime.now(),
+    );
+    ref.read(messageProvider.notifier).upsertMessage(message); // 添加消息
+    controller.clear();
+    _requestChatGPT(ref, content);
+  }
+
+  _requestChatGPT(WidgetRef ref, String content) async {
+    ref.read(chatUiProvider.notifier).setRequestLoading(true);
+    try {
+      final id = uuid.v4();
+      await chatgpt.streamChat(
+        content,
+        onSuccess: (text) {
+          final message = Message(
+            id: id,
+            content: text,
+            isUser: false,
+            timestamp: DateTime.now(),
+          );
+          ref.read(messageProvider.notifier).upsertMessage(message);
+        },
+      );
+    } catch (err) {
+      logger.e("requestChatGPT error: $err", err);
+    } finally {
+      ref.read(chatUiProvider.notifier).setRequestLoading(false);
+    }
+  }
+}
+
 class MessageItem extends StatelessWidget {
   const MessageItem({
     super.key,
@@ -132,20 +145,60 @@ class MessageItem extends StatelessWidget {
       children: [
         CircleAvatar(
           backgroundColor: message.isUser ? Colors.blue : Colors.blueGrey,
-          child: Text(
-            message.isUser ? 'A' : 'GPT',
-          ),
+          child: message.isUser
+              ? const Text(
+                  'A',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              : const Icon(
+                  Icons.android,
+                  color: Colors.white,
+                ),
         ),
         const SizedBox(
           width: 8,
         ),
         Flexible(
           child: Container(
-            margin: const EdgeInsets.only(top: 12, right: 48),
-            child: Text(message.content),
+            margin: const EdgeInsets.only(right: 48),
+            child: MessageContentWidget(
+              message: message,
+            ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class MessageContentWidget extends StatelessWidget {
+  const MessageContentWidget({
+    super.key,
+    required this.message,
+  });
+
+  final Message message;
+
+  @override
+  Widget build(BuildContext context) {
+    codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
+    return SelectionArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: MarkdownGenerator(
+          config: MarkdownConfig().copy(configs: [
+            const PreConfig().copy(wrapper: codeWrapper),
+          ]),
+          generators: [
+            latexGenerator,
+          ],
+          inlineSyntaxes: [
+            LatexSyntax(),
+          ],
+        ).buildWidgets(message.content),
+      ),
     );
   }
 }
