8f740754860872b884cc8dc7fd7bc6e5cd959dd9
Zhiyi Jin <victoryjzy@gmail.com>
2023-06-10 15:11:40 -0700

    1. Integrate the Stream API to provide a seamless user experience, and adjust the message auto-scrolling and input box accordingly. 2. Use the Freezed tool to handle boilerplate code for data classes.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:chatgpt/widgets/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'env.dart';
-
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chatgpt/injection.dart';
 import 'package:chatgpt/models/message.dart';
+import 'package:chatgpt/states/chat_ui_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../states/messge_state.dart';
@@ -10,7 +12,7 @@ class ChatScreen extends HookConsumerWidget {
   ChatScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final messages = ref.watch(messageProvider);
+    final chatUIState = ref.watch(chatUiProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
@@ -19,21 +21,13 @@ class ChatScreen extends HookConsumerWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Expanded(
+            const Expanded(
               // 聊天消息列表
-              child: ListView.separated(
-                itemBuilder: (context, index) {
-                  return MessageItem(message: messages[index]);
-                },
-                itemCount: messages.length, // 消息数量
-                separatorBuilder: (context, index) => const Divider(
-                  // 分割线
-                  height: 16,
-                ),
-              ),
+              child: ChatMessageList(),
             ),
             // 输入框
             TextField(
+              enabled: !chatUIState.requestLoading,
               controller: _textController,
               decoration: InputDecoration(
                   hintText: 'Type a message', // 显示在输入框内的提示文字
@@ -57,19 +51,68 @@ class ChatScreen extends HookConsumerWidget {
 
   // 增加WidgetRef
   _sendMessage(WidgetRef ref, String content) {
-    final message =
-        Message(content: content, isUser: true, timestamp: DateTime.now());
-    ref.read(messageProvider.notifier).addMessage(message); // 添加消息
+    final message = Message(
+      id: uuid.v4(),
+      content: content,
+      isUser: true,
+      timestamp: DateTime.now(),
+    );
+    ref.read(messageProvider.notifier).upsertMessage(message); // 添加消息
     _textController.clear();
     _requestChatGPT(ref, content);
   }
 
   _requestChatGPT(WidgetRef ref, String content) async {
-    final res = await chatgpt.sendChat(content);
-    final text = res.choices.first.message?.content ?? "";
-    final message =
-        Message(content: text, isUser: false, timestamp: DateTime.now());
-    ref.read(messageProvider.notifier).addMessage(message);
+    ref.read(chatUiProvider.notifier).setRequestLoading(true);
+    try {
+      final id = uuid.v4();
+      await chatgpt.streamChat(
+        content,
+        onSuccess: (text) {
+          final message = Message(
+            id: id,
+            content: text,
+            isUser: false,
+            timestamp: DateTime.now(),
+          );
+          ref.read(messageProvider.notifier).upsertMessage(message);
+        },
+      );
+    } catch (err) {
+      logger.e("requestChatGPT error: $err", err);
+    } finally {
+      ref.read(chatUiProvider.notifier).setRequestLoading(false);
+    }
+  }
+}
+
+class ChatMessageList extends HookConsumerWidget {
+  const ChatMessageList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final messages = ref.watch(messageProvider);
+    final listController = useScrollController();
+    ref.listen(messageProvider, (previous, next) {
+      Future.delayed(const Duration(milliseconds: 50), () {
+        listController.jumpTo(
+          listController.position.maxScrollExtent,
+        );
+      });
+    });
+    return ListView.separated(
+      controller: listController,
+      itemBuilder: (context, index) {
+        return MessageItem(message: messages[index]);
+      },
+      itemCount: messages.length, // 消息数量
+      separatorBuilder: (context, index) => const Divider(
+        // 分割线
+        height: 16,
+      ),
+    );
   }
 }
 
@@ -84,6 +127,8 @@ class MessageItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         CircleAvatar(
           backgroundColor: message.isUser ? Colors.blue : Colors.blueGrey,
@@ -94,7 +139,12 @@ class MessageItem extends StatelessWidget {
         const SizedBox(
           width: 8,
         ),
-        Text(message.content),
+        Flexible(
+          child: Container(
+            margin: const EdgeInsets.only(top: 12, right: 48),
+            child: Text(message.content),
+          ),
+        ),
       ],
     );
   }
